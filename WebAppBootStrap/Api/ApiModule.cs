d6323f1214b3158dcd1157c68ea80f12d77f7c0d
kangwl <kangwl2009@163.com>
2015-07-14 17:34:23 +0800

    kangwl

---
+++ WebAppBootStrap/Api/ApiModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppBS.Api {
+    public class ApiModule:IHttpModule {
+        public void Init(HttpApplication context) {
+            context.BeginRequest += context_BeginRequest;
+        }
+
+        void context_BeginRequest(object sender, EventArgs e) {
+            HttpApplication application = sender as HttpApplication;
+            string absPath = application.Request.Url.AbsolutePath.ToLower();
+            var arr = absPath.Split('/');
+            int a = 0;
+            if (arr.Contains("api")) {
+                a = 12;
+                application.Context.RewritePath("/view/user/list.aspx");
+            }
+            string b = a.ToString();
+        }
+
+        public void Dispose() {
+            throw new NotImplementedException();
+        }
+    }
+}
