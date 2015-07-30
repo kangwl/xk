44ee0298025cff1c3a9c0d932f52e188c8bb6fc7
kangwl <kangwl2009@163.com>
2015-07-30 09:06:07 +0800

    kangwl

--- WebAppBootStrap/Global.asax.cs
+++ WebAppBootStrap/Global.asax.cs
@@ -30,8 +30,8 @@ namespace WebAppBS {
 
             AppStart.Log4net.logger.ErrorFormat("客户机IP：{0},错误地址:{1},信息：{2}", Request.UserHostAddress, Request.Url,
                 HttpContext.Current.Error);
-            HttpContext.Current.Server.ClearError();
-            HttpContext.Current.Response.Write("系统出错");
+           // HttpContext.Current.Server.ClearError();
+            //HttpContext.Current.Response.Write("系统出错");
         }
 
         protected void Session_End(object sender, EventArgs e) {
